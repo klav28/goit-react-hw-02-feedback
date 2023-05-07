41ae061a1e0e36099e55004c23873d1c2f3a75e7
Konstantin Leschenko <klav28@gmail.com>
2023-05-07 10:51:36 +0300

    1st

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// import { Header } from "./components/Header";
+import { ThemeProvider } from 'styled-components';
+// import { Todos } from "./components/Todos";
+import { Component } from 'react';
+// import { DARK, LIGHT } from "./constants/theme";
+// import { theme } from "./theme";
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <p>Please leave your feedback</p>
+        <button>Good</button>
+        <button>Neutral</button>
+        <button>Bad</button>
+      </div>
+    );
+  }
+}
